70f4db2df983a6eb4acaa191cccf75b2426bb141
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-01 23:52:58 +0900

    build: bin/rails g active_admin:resource Task
          create  app/admin/tasks.rb

---
+++ app/admin/tasks.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+ActiveAdmin.register Task do
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  # permit_params :name, :project_id, :status
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:name, :project_id, :status]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+end
