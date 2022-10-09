fae835b26c77efccc6bad02b7fe0598e17a1118b
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-09 21:03:26 +0900

    feat: Sentry の設定を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+  before_action :configure_sentry
   before_action :authenticate_account!
+
+  def configure_sentry
+    Sentry.set_user(id: current_account.id, email: current_account.email) if account_signed_in?
+    Sentry.set_extras(params: params.to_unsafe_h, url: request.url)
+  end
 end
