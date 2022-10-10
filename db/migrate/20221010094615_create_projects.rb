64e36e11d71cf144476bd0d2f39c5df7286e37bb
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-10 19:00:15 +0900

    feat: projects の name に null 制約を追加

--- db/migrate/20221010094615_create_projects.rb
+++ db/migrate/20221010094615_create_projects.rb
@@ -3,7 +3,7 @@
 class CreateProjects < ActiveRecord::Migration[7.0]
   def change
     create_table :projects do |t|
-      t.string :name
+      t.string :name, null: false
 
       t.timestamps
     end
