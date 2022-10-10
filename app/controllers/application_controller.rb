f4501b6c63721e49673f16b315fc599b1dd81e9a
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-10 13:09:38 +0900

    fix: ログインしていない状態で admin にアクセスした際に new_account_session_path にリダイレクトされないように修正
    devise や active_admin は ApplicationController を共有していて active_admin で実行している before_action よりも先に ApplicationController の before_action が実行されるため authentication_method(== authenticate_admin_user!) が実行される前に authenticate_account! が実行されて admin でも new_account_session_path にリダイレクトされてしまっていた

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@
 
 class ApplicationController < ActionController::Base
   before_action :configure_sentry
-  before_action :authenticate_account!
+  before_action :authenticate_account!, unless: -> { params[:controller].start_with?(/admin/) }
 
   def configure_sentry
     Sentry.set_user(id: current_account.id, email: current_account.email) if account_signed_in?
