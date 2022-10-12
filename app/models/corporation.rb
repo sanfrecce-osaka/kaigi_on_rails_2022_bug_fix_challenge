079b8d4cd7ff614bedba18eaf0592dab2ca6f579
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-12 20:28:44 +0900

    feat: Corporation に status の enum 定義を追加

--- app/models/corporation.rb
+++ app/models/corporation.rb
@@ -3,4 +3,6 @@
 class Corporation < ApplicationRecord
   has_many :accounts, dependent: :destroy
   has_many :projects, dependent: :destroy
+
+  enum :status, { reviewed: 'reviewed', approved: 'approved', rejected: 'rejected' }, default: :reviewed
 end
