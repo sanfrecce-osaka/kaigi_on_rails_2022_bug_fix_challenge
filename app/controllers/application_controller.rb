1e0b70f6181a395d10d4dbd813f2b27f0ca18a78
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-12 22:19:06 +0900

    feat: ログイン後の path を設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,14 @@ class ApplicationController < ActionController::Base
     Sentry.set_user(id: current_account.id, email: current_account.email) if account_signed_in?
     Sentry.set_extras(params: params.to_unsafe_h, url: request.url)
   end
+
+  def after_sign_in_path_for(account)
+    return super if account.kind_of? AdminUser
+
+    flash.clear
+    case account.corporation
+    in ->(corporation) { corporation.free? }    then tasks_path
+    in ->(corporation) { corporation.premium? } then projects_path
+    end
+  end
 end
