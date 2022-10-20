25cfe34a8c70c2e0ee6ad36e9f9a27cfceb2069f
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-21 03:00:52 +0900

    feat: tasks に null 制約と status のデフォルト値を追加

--- db/migrate/20221010102727_create_tasks.rb
+++ db/migrate/20221010102727_create_tasks.rb
@@ -3,9 +3,9 @@
 class CreateTasks < ActiveRecord::Migration[7.0]
   def change
     create_table :tasks do |t|
-      t.string :name
+      t.string :name, null: false
       t.references :project, null: false, foreign_key: true
-      t.string :status
+      t.string :status, null: false, default: 'ToDo'
 
       t.timestamps
     end
