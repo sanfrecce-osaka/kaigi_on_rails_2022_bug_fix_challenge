0ab5ee9607b47c3906e2c3eec03b6dd38b2714cd
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-21 03:30:09 +0900

    feat: corporations の plan に null 制約とデフォルト値を追加

--- db/migrate/20221012124739_add_plan_to_corporations.rb
+++ db/migrate/20221012124739_add_plan_to_corporations.rb
@@ -2,6 +2,6 @@
 
 class AddPlanToCorporations < ActiveRecord::Migration[7.0]
   def change
-    add_column :corporations, :plan, :string
+    add_column :corporations, :plan, :string, null: false, default: 'free'
   end
 end
