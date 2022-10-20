f00dbee26f0e29f4be1654027c6376c8a819d89c
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-21 03:28:50 +0900

    feat: admin/accounts に permit params の設定を追加

--- app/admin/accounts.rb
+++ app/admin/accounts.rb
@@ -6,7 +6,7 @@ ActiveAdmin.register Account do
   #
   # Uncomment all parameters which should be permitted for assignment
   #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :corporation_id
+  permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email, :corporation_id
   #
   # or
   #
