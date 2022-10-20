2cae6a7ce565c307fec57be9b2de055b0c75577b
masatoshi_moritsuka <yakiyaki_ikayaki@yahoo.co.jp>
2022-10-21 03:24:18 +0900

    feat: corporations に null 制約と status のデフォルト値を追加

--- db/migrate/20221010113619_create_corporations.rb
+++ db/migrate/20221010113619_create_corporations.rb
@@ -3,8 +3,8 @@
 class CreateCorporations < ActiveRecord::Migration[7.0]
   def change
     create_table :corporations do |t|
-      t.string :name
-      t.string :status
+      t.string :name, null: false
+      t.string :status, null: false, default: 'reviewed'
 
       t.timestamps
     end
